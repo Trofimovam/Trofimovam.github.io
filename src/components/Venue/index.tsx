6d3f7e43ee8eace4e885db746f9d43d65398cced
Trofimovam <trofimovam7396@gmail.com>
2022-08-10 17:42:40 +0300

    delete location

--- src/components/Venue/index.tsx
+++ src/components/Venue/index.tsx
@@ -4,11 +4,11 @@ import { ButtonLink, Title, Text, Container } from "./styled";
 export default function Venue() {
   return (
     <Container id='venue'>
-      <Title>Место регистрации</Title>
+      {/* <Title>Место регистрации</Title>
       <Text>город Саранск, улица Советская д. 47А</Text>
       <ButtonLink href='https://yandex.ru/maps/-/CCURM8rVdD' target='blank'>
         Показать на карте
-      </ButtonLink>
+      </ButtonLink> */}
       <Title>Место торжества</Title>
       <Text>город Саранск, улица Коммунистическая д. 35</Text>
       <Text>Мы будем ждать Вас в 16:00</Text>
